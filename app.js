b6fe3140361c669a2af139456a262d5421468d7f
Alejandro Mejia <mejia.alejandro1@outlook.com>
2022-09-15 16:41:56 -0400

    nodes

---
+++ app.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
